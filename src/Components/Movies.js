b405e72294d113bd11d7b327c28891409d5e4e9f
saisrivenkat <50038114+saisrivenkat@users.noreply.github.com>
2021-03-21 12:29:48 +0530

    added styles

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Card, Row, Col } from 'react-bootstrap'
 import '../App.css'
-import { Link } from "react-router-dom";
+
 
 
 function Movies({ movies }) {
@@ -13,18 +12,6 @@ function Movies({ movies }) {
         return overview
     }
 
-
-    const color = (vote) => {
-        if (vote < 5) {
-            return "red";
-        }
-        else if (vote > 5 & vote < 7) {
-            return "orange"
-        }
-        else {
-            return "green"
-        }
-    }
     return (
         <React.Fragment>
             <div className="movies">
@@ -46,51 +33,7 @@ function Movies({ movies }) {
                             </div>
                         </div>
                     ))}
-
-
-
-
                 </div>
-
-
-                {/*<Row>
-                    {movies.map(ul => (
-                        <Col >
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={url + ul.poster_path} alt={ul.poster_path} />
-                                <Card.Body>
-                                    <Card.Title>
-                                        <Row>
-                                            <Col>
-                                                <span> {ul.title}</span>
-                                                <span className={color(ul.vote_average)}>{ul.vote_average}</span>
-                                            </Col>
-                                        </Row>
-                                        <a href={urlchange(ul.id)}>More</a>
-
-                                        <Link
-                                            className="btn btn-primary"
-                                            to={{
-                                                pathname: "/overview",
-                                                state: {
-                                                    overview: ul.overview,
-                                                    title: ul.title,
-                                                    id: ul.id
-
-                                                }
-                                            }}
-                                        >
-                                            more...
-        </Link>
-
-
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-                                        */}
             </div>
         </React.Fragment>
     );
